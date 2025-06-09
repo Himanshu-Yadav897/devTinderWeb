d66565d36bd5890f20614ced76435ed2d7dcb978
Himanshu Yadav <hy897433@gmail.com>
2025-06-09 18:33:45 +0530

    implement redux store

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { addUser } from "../utils/userSlice";
+import { useDispatch } from "react-redux";
+import {  useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-export default Login
+  const handleLogin = async () => {
+    try {
+      // Basic validation
+
+      const res = await axios.post(
+        BASE_URL + "/login",
+        {
+          emailId: email,
+          password,
+        },
+        { withCredentials: true }
+      );
+
+      // console.log(res.data);
+
+      dispatch(addUser(res.data));
+
+      return navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-700">
+      <div className="bg-black p-8 rounded-2xl shadow-md w-full max-w-md">
+        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+
+        <div className="mb-4">
+          <label className="block mb-1 text-sm font-medium">
+            Email : {email}
+          </label>
+          <input
+            type="email"
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="example@email.com"
+          />
+        </div>
+
+        <div className="mb-6">
+          <label className="block mb-1 text-sm font-medium">
+            Password : {password}
+          </label>
+          <input
+            type="password"
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="••••••••"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full cursor-pointer bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
+          onClick={handleLogin}
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
