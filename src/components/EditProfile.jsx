7792f797f55296303a14f2f854a4d01604f57a5b
Himanshu Yadav <hy897433@gmail.com>
2025-06-16 12:49:32 +0530

    bug 2 : usercrad naming

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import UserCard from "./UserCard";
+import UserCard from "./Usercard";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch } from "react-redux";
